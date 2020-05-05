3c7693e5a563a5090489186859efe4e421ff75fe
corsa <corsa@LAPTOP-1QBGBL4D.station>
2020-05-05 17:35:51 +0200

    Version 1.0

---
+++ src/Prova.java
@@ -0,0 +1,20 @@
+//version 1
+public class Prova {
+	private int b;
+
+	public Prova() {
+		b = 5;
+	}
+
+	public int getNum() {
+		return b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Prova p = new Prova();
+		System.out.println("Ciao!!");
+		System.out.println(p.getNum());
+	}
+
+}
